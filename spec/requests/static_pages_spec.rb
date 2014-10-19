542d68c647f54f893a47f6473d1bbe0fc409fe6d
Laurie Morse <lmorse@sbcglobal.net>
2014-10-19 11:52:33 -0500

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,59 +6,35 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-# THIS IS NOT WORKING FOR SOME REASON.  COULD IT BE A VERSION ISSUE?  MY GEMFILE IS NOW TOTALLY DIFFERENT THAN THE BOOK.
-#    it "should have the base title" do
-#      visit '/static_pages/home'
-#      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
-#    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
-	
-	it "should have the content 'Sample App'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+	
+	it "should have the base title" do
+		visit '/static_pages/home'
+		expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+	end
 
-# THIS DOES WORK.  NOT SURE WHY I WAS USING THIS IN THE PREVIOUS ASSIGNMENT.  WAS IT DIFFERENT IN THE PREVIOUS ONLINE BOOK?
-#    it "should have the title 'Home'" do
-#      visit '/static_pages/home'
-#      expect(page).to have_title("#{base_title} | Home")
-#    end
+	# SAME THING USING THE BASE_TITLE VARIABLE
+	it "should have the base title" do
+		visit '/static_pages/home'
+		expect(page).to have_title("#{base_title}")
+	end
 
-# THIS DOES WORK.  NOT SURE WHY I WAS USING THIS IN THE PREVIOUS ASSIGNMENT.  WAS IT DIFFERENT IN THE PREVIOUS ONLINE BOOK?
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
+	it "should not have a custom page title" do
+		visit '/static_pages/home'
+		expect(page).not_to have_title('| Home')
+	end
   end
 
   describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-# THIS IS NOT WORKING FOR SOME REASON.  COULD IT BE A VERSION ISSUE?  MY GEMFILE IS NOW TOTALLY DIFFERENT THAN THE BOOK.
-#    it "should have the title 'Help'" do
-#      visit '/static_pages/help'
-#      page.should have_selector('title', :text => "#{base_title} | Help")
-#    end
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
 
-# THIS DOES WORK.  NOT SURE WHY I WAS USING THIS IN THE PREVIOUS ASSIGNMENT.  WAS IT DIFFERENT IN THE PREVIOUS ONLINE BOOK?
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("#{base_title} | Help")
@@ -67,23 +43,11 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-# THIS IS NOT WORKING FOR SOME REASON.  COULD IT BE A VERSION ISSUE?  MY GEMFILE IS NOW TOTALLY DIFFERENT THAN THE BOOK.
-#    it "should have the title 'About Us'" do
-#      visit '/static_pages/about'
-#      page.should have_selector('title', :text => "#{base_title} | About Us")
-#    end
-
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
 
-# THIS DOES WORK.  NOT SURE WHY I WAS USING THIS IN THE PREVIOUS ASSIGNMENT.  WAS IT DIFFERENT IN THE PREVIOUS ONLINE BOOK?
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("#{base_title} | About Us")
@@ -92,23 +56,11 @@ describe "Static pages" do
 
   describe "Contact page" do
 
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-# THIS IS NOT WORKING FOR SOME REASON.  COULD IT BE A VERSION ISSUE?  MY GEMFILE IS NOW TOTALLY DIFFERENT THAN THE BOOK.
-#    it "should have the title 'Contact'" do
-#      visit '/static_pages/contact'
-#      page.should have_selector('title', :text => "#{base_title} | Contact")
-#    end
-
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
 
-# THIS DOES WORK.  NOT SURE WHY I WAS USING THIS IN THE PREVIOUS ASSIGNMENT.  WAS IT DIFFERENT IN THE PREVIOUS ONLINE BOOK?
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_title("#{base_title} | Contact")
